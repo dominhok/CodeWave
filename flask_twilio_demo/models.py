3431ea0b07d36e6866ede74ebc9f6267c8501471
dominhok <ilw2y123@naver.com>
2025-04-12 15:25:00 +0900

    made phone call & response

--- flask_twilio_demo/models.py
+++ flask_twilio_demo/models.py
@@ -20,6 +20,7 @@ class User(Base):
     has_guardian = Column(Boolean, default=False) # 보호자 연결 여부
     guardian_phone_number = Column(String, nullable=True) # 보호자 번호
     wants_info_call = Column(Boolean, default=True) # 안내 전화 여부
+    is_visually_impaired = Column(Boolean, default=False, nullable=False) # 시각장애 여부 플래그 추가
 
 # Dependency to get DB session
 def get_db():
